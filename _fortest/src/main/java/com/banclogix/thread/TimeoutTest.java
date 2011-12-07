95d626c65874ac4653a54e0aba3681de5b806eba
zhengzh <zhengzh@jhconn.net>
2011-12-07 10:15:39 +0800

    df

---
+++ _fortest/src/main/java/com/banclogix/thread/TimeoutTest.java
@@ -0,0 +1,23 @@
+package com.banclogix.thread;
+
+public class TimeoutTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+//		//初始化超时类
+//		TimeoutThread t = new TimeoutThread(5000,new TimeoutException("超时"));
+//
+//		try{
+//			t.start();
+//			//.....要检测超时的程序段....
+//			t.cancel();
+//		}catch (TimeoutException e){
+//			//...对超时的处理...
+//		}
+//		//TimeoutException可以更换为其他未检查异常类。 
+
+	}
+
+}
